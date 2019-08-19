678e2eb2a674fad94cce53fffdcb94d586deaa46
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2019-08-19 16:26:38 +0200

    fix

---
+++ src/sql/init.sql
@@ -0,0 +1,22 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION clickhouse_fdw" to load this file. \quit
+
+CREATE FUNCTION clickhousedb_fdw_handler()
+RETURNS fdw_handler
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION clickhousedb_raw_query(TEXT, TEXT DEFAULT 'http://localhost:8123')
+RETURNS TEXT
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION clickhousedb_fdw_validator(text[], oid)
+RETURNS VOID
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+
+CREATE FOREIGN DATA WRAPPER clickhouse_fdw
+	HANDLER clickhousedb_fdw_handler
+	VALIDATOR clickhousedb_fdw_validator;
